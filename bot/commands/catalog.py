ebec77df48192ddd8df2dc27211d4e6bd4904d05
PedroPiveta <ppivetabarrotti208@gmail.com>
2024-11-12 20:20:55 -0300

    feat: cart command

--- bot/commands/catalog.py
+++ bot/commands/catalog.py
@@ -1,9 +1,9 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, MenuButton
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ContextTypes
 from io import BytesIO
 import requests
 
-from config import API_URL, IMG_PREFIX
+from config import API_URL, IMG_PREFIX, cart
 
 PRODUCT_QUANTITY_CALLBACK = "catalog=product-quantity"
 MINUS_PRODUCT_CALLBACK = "catalog=minus-product_"
@@ -11,22 +11,23 @@ PLUS_PRODUCT_CALLBACK = "catalog=plus-product_"
 OPEN_CATALOG_CALLBACK = "catalog=open-catalog"
 OPEN_CART_CALLBACK = "catalog=open-cart"
 
+# Função para construir o teclado do produto
 def build_keyboard(product, cart_item=None):
     keyboard = []
 
     if cart_item:
         keyboard.append([
             InlineKeyboardButton(text='➖', callback_data=f'{MINUS_PRODUCT_CALLBACK}{product["id"]}'),
-            InlineKeyboardButton(text=f'{cart_item.quantity} pcs.', callback_data=PRODUCT_QUANTITY_CALLBACK),
+            InlineKeyboardButton(text=f'{cart_item["quantity"]} pcs.', callback_data=PRODUCT_QUANTITY_CALLBACK),
             InlineKeyboardButton(text='➕', callback_data=f'{PLUS_PRODUCT_CALLBACK}{product["id"]}')
         ])
         keyboard.append([
-            InlineKeyboardButton(text="Catalog", callback_data=OPEN_CATALOG_CALLBACK),
-            InlineKeyboardButton(text="Cart", callback_data=OPEN_CART_CALLBACK)
+            InlineKeyboardButton(text="Catálogo", callback_data=OPEN_CATALOG_CALLBACK),
+            InlineKeyboardButton(text="Carrinho", callback_data=OPEN_CART_CALLBACK)
         ])
     else:
         keyboard.append([
-            InlineKeyboardButton(text=f'💵 Price: {product["price"]} $ 🛍 Add to cart', callback_data=f'{PLUS_PRODUCT_CALLBACK}{product["id"]}')
+            InlineKeyboardButton(text=f'💵 Preço: {product["price"]} $ 🛍 Adicionar ao carrinho', callback_data=f'{PLUS_PRODUCT_CALLBACK}{product["id"]}')
         ])
 
     return InlineKeyboardMarkup(keyboard)
@@ -43,7 +44,19 @@ def build_category_keyboard(categories):
 
 async def handle_callback_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+    user_id = query.from_user.id
     await query.answer()
+
+    data = query.data
+
+    if query.data == 'checkout':
+        await query.message.reply_text("Indo para o checkout...")
+        # Lógica de checkout aqui
+    elif query.data == 'clear_cart':
+        # Limpar o carrinho do usuário
+        if user_id in cart:
+            cart[user_id].clear()
+        await query.message.reply_text("Seu carrinho foi limpo.")
 
     if query.data.startswith("category="):
         category_id = query.data.split("=")[1]
@@ -56,25 +69,20 @@ async def handle_callback_query(update: Update, context: ContextTypes.DEFAULT_TY
             if products and isinstance(products, list):
                 for product in products:
                     if all(k in product for k in ['name', 'price', 'id', 'photo_url']):
-                        keyboard = build_keyboard(product)
+                        keyboard = build_keyboard(product, cart.get(query.from_user.id, {}).get(str(product['id'])))
                         image_response = requests.get(f"{IMG_PREFIX}{product['photo_url']}")
                         if image_response.status_code == 200:
                             photo = BytesIO(image_response.content)
                             await query.message.reply_photo(photo=photo)
-                        else:
-                            await query.message.reply_text(f"Product: {product['name']}\nPrice: {product['price']} $ (No Image Available)", reply_markup=keyboard)
                         await query.message.reply_text(
-                            text=f"{product['name']}\nPrice: {product['price']} $",
+                            text=f"{product['name']}\nPreço: {product['price']} $",
                             reply_markup=keyboard
                         )
-                    else:
-                        await query.message.reply_text("Some product data is incomplete.")
             else:
-                await query.message.reply_text("No products found for this category.")
+                await query.message.reply_text("Nenhum produto encontrado para esta categoria.")
         else:
-            await query.message.reply_text("Failed to load products for the selected category.")
+            await query.message.reply_text("Falha ao carregar produtos para a categoria selecionada.")
     
-    # Process other callbacks for cart operations (like adding/removing products)
     elif query.data.startswith("catalog="):
         await handle_cart_operations(query)
 
@@ -87,19 +95,42 @@ async def catalog_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if categories and isinstance(categories, list):
             await update.message.reply_text("Catalog:", reply_markup=build_category_keyboard(categories=categories))
         else:
-            await update.message.reply_text("No categories available.")
+            await update.message.reply_text("Sem categorias cadastradas.")
     else:
-        await update.message.reply_text("Failed to load catalog.")
+        await update.message.reply_text("Algo deu errado ao buscar o catálogo.")
     
     print(response.json()) if response.status_code == 200 else print("Error loading categories")
 
 async def handle_cart_operations(query):
     data = query.data
+    chat_id = query.message.chat_id
+
+    if chat_id not in cart:
+        cart[chat_id] = {}
+
     if data.startswith(MINUS_PRODUCT_CALLBACK):
         product_id = data.split('_')[1]
-        # Lógica para diminuir a quantidade do produto no carrinho
+        if product_id in cart[chat_id]:
+            if cart[chat_id][product_id]['quantity'] > 1:
+                cart[chat_id][product_id]['quantity'] -= 1
+            else:
+                del cart[chat_id][product_id]
         await query.answer(f"Product {product_id} decreased.")
     elif data.startswith(PLUS_PRODUCT_CALLBACK):
         product_id = data.split('_')[1]
-        # Lógica para aumentar a quantidade do produto no carrinho
-        await query.answer(f"Product {product_id} increased.")
+        product_name = query.message.text.split('\n')[0]
+        product_price = float(query.message.text.split('\n')[1].split(' ')[1])
+        
+        if product_id in cart[chat_id]:
+            cart[chat_id][product_id]['quantity'] += 1
+        else:
+            cart[chat_id][product_id] = {
+                'quantity': 1,
+                'product_name': product_name,
+                'price': product_price
+            }
+        
+        await query.message.reply_text(f"O produto {product_name} foi adicionado ao carrinho. Quantidade: {cart[chat_id][product_id]['quantity']}")
+
+        await query.answer(f"Product {product_name} increased.")
+        print(cart)
--- bot/config.py
+++ bot/config.py
@@ -4,3 +4,5 @@ TOKEN: Final = "7715292562:AAHBcryB9C1m26GWjTSRe4GLkiFPdE14AuM"
 BOT_USERNAME = "@piveta_delivery_bot"
 API_URL = "http://localhost:5000/api"
 IMG_PREFIX = "http://localhost:5000/static/img/produtos/"
+
+cart = {}
