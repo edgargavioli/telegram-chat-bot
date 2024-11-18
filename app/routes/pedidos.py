cab4b5cd5845facd57d07b3a5d25e89c6b773d98
JESUISzin <guto3210123@gmail.com>
2024-11-18 20:35:51 -0300

    feat: rmk pedidos
    cadastro, edição e remoção de pedidos pela área adm foi readaptada para manipular a tabela orders_items no banco

--- app/routes/pedidos.py
+++ app/routes/pedidos.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from . import login
 from api.models.orders import Order
+from api.models.products import Product
 
 bp = Blueprint('pedidos', __name__, url_prefix='/pedidos')
 
@@ -25,7 +26,8 @@ def editar_pedido(id):
     if login.current_user.is_authenticated:
         order = Order.query.get_or_404(id)
         saved_date, saved_time = str(order.created_date).split(' ')
-        return render_template('pages/pedidos/editar_pedido.html', order=order, saved_date=saved_date, saved_time=saved_time)
+        all_products = Product.query.all()
+        return render_template('pages/pedidos/editar_pedido.html', order=order, saved_date=saved_date, saved_time=saved_time, all_products=all_products)
     else:
         flash('Por favor, faça login para acessar esta página.', 'warning')
         return redirect(url_for('login.login'))
